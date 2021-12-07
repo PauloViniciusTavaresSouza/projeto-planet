76730836af96ac484b040f8f1f1af22a6dc85a65
Paulo <pauloviniciustavaressouza@gmail.com>
2021-12-07 16:14:38 -0300

    Commit de teste - 07/12/2021

--- src/Components/Template/Destination/Planets/HomePlanets.jsx
+++ src/Components/Template/Destination/Planets/HomePlanets.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./style.css"
 import {FundoPlanet, DivTituloPlanets, TituloPlanetas,
 NavLinkMoon, PaginationNav, UlNavLink, LiNavLink} from "./styledPlanets"
-import { Link } from "react-router-dom";
 import Menu from "../../Menu/Menu";
 import Moon from "../Planets/Moon/Moon"
 
@@ -11,6 +10,7 @@ import Moon from "../Planets/Moon/Moon"
 function activeClass(clicado) { 
     const clicaveis = document.querySelectorAll(".Inativo");
     clicaveis.forEach(el => el.classList.toggle("ativo", el === clicado))
+    console.log('ativado')
 }
 
 
@@ -23,14 +23,14 @@ const HomePlanets = () => {
             <TituloPlanetas>Pick your destination</TituloPlanetas>
             <NavLinkMoon>
                 <UlNavLink>
-                    <LiNavLink className="ativo" onclick="activeClass(this)"> <PaginationNav>MOON</PaginationNav> </LiNavLink>
+                    <LiNavLink className="ativo" onclick={activeClass(this)}> <PaginationNav>MOON</PaginationNav> </LiNavLink>
                     <LiNavLink className="ativo" onclick="activeClass(this)"> <PaginationNav>MARS</PaginationNav> </LiNavLink>
-                    <LiNavLink className="Inativo" onclick="activeClass(this)"> <PaginationNav>EUROPA</PaginationNav> </LiNavLink>
-                    <LiNavLink className="Inativo" onclick="activeClass(this)"> <PaginationNav>TITAN</PaginationNav> </LiNavLink>
+                    <LiNavLink className="ativo" onclick="activeClass(this)"> <PaginationNav>EUROPA</PaginationNav> </LiNavLink>
+                    <LiNavLink className="ativo" onclick="activeClass(this)"> <PaginationNav>TITAN</PaginationNav> </LiNavLink>
                 </UlNavLink>
             </NavLinkMoon>
         </DivTituloPlanets>
-
+        <Moon />
 
         </FundoPlanet>
 
